9dc6d6f6396d69e2a4401fda57aec0748145f8d0
Joseph2912 <elsjoseph@hotmail.com>
2024-03-23 23:58:28 -0500

    betsy a probar

---
+++ 72 horas/Assets/Scripts/Portal.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public Transform destination; // Punto de destino al que se teletransportará el jugador
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // Verificar si el objeto que entra al portal es el jugador
+        {
+            TeleportPlayer(other.transform);
+        }
+    }
+
+    private void TeleportPlayer(Transform playerTransform)
+    {
+        if (destination != null)
+        {
+            playerTransform.position = destination.position; // Teletransportar al jugador al punto de destino
+        }
+        else
+        {
+            Debug.LogWarning("¡El portal no tiene un punto de destino asignado!");
+        }
+    }
+}
