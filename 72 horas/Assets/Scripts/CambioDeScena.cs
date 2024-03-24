6d4d095fecf46069ca0872b021b8ae5f05bc9159
Betsy <89090441+BetsyAlejandra@users.noreply.github.com>
2024-03-23 21:40:25 -0500

    skjfñigihlusnzm

---
+++ 72 horas/Assets/Scripts/CambioDeScena.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioDeScena : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("Dungeon Top Down");
+    }
+
+    public void Instrucciones()
+    {
+        SceneManager.LoadScene("Instrucciones");
+    }
+
+    public void Salir()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+}
